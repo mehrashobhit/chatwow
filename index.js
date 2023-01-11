1cf3ac7f5720f468eefbb2f9e94b6035a1062237
mehrashobhit <49863919+mehrashobhit@users.noreply.github.com>
2023-01-11 14:29:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
 app.listen(process.env.PORT || 3000)
+
+
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion () {
+const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "write a mongoose query to fetch users with their recent chat with all other users",
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return completion.data.choices[0].text;
+}
+app.all('/', (req, res) => {
+const resp= runCompletion();
+res.send(resp)
+})
